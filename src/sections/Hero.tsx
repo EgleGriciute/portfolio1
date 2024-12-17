10d364a0ba3ba24a6e126ed99603c51bd5b03ea5
Egle Griciute <egllegriciute@gmail.com>
2024-12-17 16:55:55 +0200

    changed ' symbol

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -157,7 +157,7 @@ export const HeroSection = () => {
             className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl z-10"
           >
             <span>👋</span>
-            <span className="font-semibold">Let's connect</span>
+            <span className="font-semibold">Let&apos;s connect</span>
           </a>
         </div>
       </div>
